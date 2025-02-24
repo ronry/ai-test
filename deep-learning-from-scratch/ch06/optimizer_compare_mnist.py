4ecb9ff676406025730421dd7565a49976c7c889
jinliang.lvjl <jinliang.lvjl@alibaba-inc.com>
2025-02-24 15:22:38 +0800

    补全import路径

--- deep-learning-from-scratch/ch06/optimizer_compare_mnist.py
+++ deep-learning-from-scratch/ch06/optimizer_compare_mnist.py
@@ -16,40 +16,37 @@ batch_size = 128
 max_iterations = 2000
 
 
-# 1:进行实验的设置==========
 optimizers = {}
 optimizers['SGD'] = SGD()
 optimizers['Momentum'] = Momentum()
 optimizers['AdaGrad'] = AdaGrad()
 optimizers['Adam'] = Adam()
-#optimizers['RMSprop'] = RMSprop()
 
-# networks = {}
-# train_loss = {}
-# for key in optimizers.keys():
-#     networks[key] = MultiLayerNet(
-#         input_size=784, hidden_size_list=[100, 100, 100, 100],
-#         output_size=10)
-#     train_loss[key] = []
-#
-# # 2:开始训练==========
-# for i in range(max_iterations):
-#     batch_mask = np.random.choice(train_size, batch_size)
-#     x_batch = x_train[batch_mask]
-#     t_batch = t_train[batch_mask]
-#
-#     for key in optimizers.keys():
-#         grads = networks[key].gradient(x_batch, t_batch)
-#         optimizers[key].update(networks[key].params, grads)
-#
-#         loss = networks[key].loss(x_batch, t_batch)
-#         train_loss[key].append(loss)
-#
-#     if i % 100 == 0:
-#         print("===========" + "iteration:" + str(i) + "===========")
-#         for key in optimizers.keys():
-#             loss = networks[key].loss(x_batch, t_batch)
-#             print(key + ":" + str(loss))
+networks = {}
+train_loss = {}
+for key in optimizers.keys():
+    networks[key] = MultiLayerNet(
+        input_size=784, hidden_size_list=[100, 100, 100, 100],
+        output_size=10)
+    train_loss[key] = []
+
+for i in range(max_iterations):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    for key in optimizers.keys():
+        grads = networks[key].gradient(x_batch, t_batch)
+        optimizers[key].update(networks[key].params, grads)
+
+        loss = networks[key].loss(x_batch, t_batch)
+        train_loss[key].append(loss)
+
+    if i % 100 == 0:
+        print("===========" + "iteration:" + str(i) + "===========")
+        for key in optimizers.keys():
+            loss = networks[key].loss(x_batch, t_batch)
+            print(key + ":" + str(loss))
 
 
 networks = {}
